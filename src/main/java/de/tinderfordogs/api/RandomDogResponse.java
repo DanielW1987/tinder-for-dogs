20f0a3db0469114df222719c738123d5bf0ebb3e
DanielWagner87 <daniel.wagner@europace.de>
2021-05-05 21:15:48 +0200

    add some tests

--- src/main/java/de/tinderfordogs/api/RandomDogResponse.java
+++ src/main/java/de/tinderfordogs/api/RandomDogResponse.java
@@ -7,6 +7,11 @@ public class RandomDogResponse {
 
   public RandomDogResponse() {}
 
+  public RandomDogResponse(String message, String status) {
+    this.message = message;
+    this.status = status;
+  }
+
   public String getMessage() {
     return message;
   }
