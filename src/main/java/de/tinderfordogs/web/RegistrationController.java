fd7e445affaffe8e2768218a2cc69392474edcbc
DanielWagner87 <daniel.wagner@europace.de>
2021-05-11 13:51:53 +0200

    redirect to login page after registration

--- src/main/java/de/tinderfordogs/web/RegistrationController.java
+++ src/main/java/de/tinderfordogs/web/RegistrationController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class RegistrationController {
@@ -25,9 +26,9 @@ public class RegistrationController {
   }
 
   @PostMapping(path = Endpoints.Site.REGISTER)
-  public ModelAndView handleRegistrationRequest(RegistrationRequest registrationRequest) {
+  public RedirectView handleRegistrationRequest(RegistrationRequest registrationRequest) {
     registrationService.registerUser(registrationRequest);
-    return new ModelAndView(ViewNames.REGISTER);
+    return new RedirectView(Endpoints.Site.LOGIN);
   }
 
   @ModelAttribute(name = "registrationRequest")
